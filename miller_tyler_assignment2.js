ddc873865654f8d15a1d41ea4df9640456170b0e
Tyler <tyemiller4@gmail.com>
2013-03-14 15:01:44 -0400

    Progressing in code

---
+++ miller_tyler_assignment2.js
@@ -0,0 +1,14 @@
+//Name: Tyler Miller
+//Date: 03/14/13
+//Assignment: Project 2
+//Description: Arrays, Funchtions, A boy and his dog
+
+//Set Variables
+var pets = [
+	"Lizard", 
+	"Bird", 
+	"Spider", 
+	"Mouse", 
+	"Dog"
+];
+
